18fc018b8efcc114e3792c31fcc77e2c3cc27976
Gurin-EI <rypylook@outlook.com>
2018-12-03 19:35:04 +0500

    v0.4.1420 (change nginx.sh)

--- docker/nginx/nginx.sh
+++ docker/nginx/nginx.sh
@@ -4,9 +4,9 @@ nginx
 
 letsencrypt certonly --webroot -w /var/www/rypy -d rypy.ru -d www.rypy.ru -m rypylook@outlook.com --agree-tos
 
-service rsyslog start && service cron start && tail -f /var/log/syslog
-
 mv /nginx2.conf /etc/nginx/nginx.conf
+
+service rsyslog start && service cron start && tail -f /var/log/syslog
 
 nginx -s reload
 
