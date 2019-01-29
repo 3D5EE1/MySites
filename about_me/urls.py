734a1ef7034e3ef20167510b6cf5285dbabd7a5a
Evgeny-Gurin <rypylook@outlook.com>
2019-01-29 20:46:08 +0500

    v0.4.9201 (add license-agreement.html)

--- about_me/urls.py
+++ about_me/urls.py
@@ -18,5 +18,6 @@ from about_me import views
 
 urlpatterns = [
     path('', views.about_me, name='about_me'),
+    path('license-agreement', views.license_agreement, name='license_agreement')
 ]
 
--- about_me/views.py
+++ about_me/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def about_me(request):
     return render(request, 'about_me/about-me.html')
+
+
+def license_agreement(request):
+    return render(request, 'about_me/license-agreement.html')
