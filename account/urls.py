b7f911d6807464efb704d496e66bc2ce76623075
Gurin-EI <rypylook@outlook.com>
2019-02-02 02:53:58 +0500

    v0.4.9248 (add 400.html)

--- account/urls.py
+++ account/urls.py
@@ -14,15 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.urls import path
+from django.urls import path, re_path
 from django.conf.urls import url
 from account import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('creation/', views.creation, name='creation'),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate,
-        name='activate'),
+    re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate,
+            name='activate'),
     path('logout/', auth_views.LogoutView.as_view(next_page='profile'), name='profile_logout'),
 ]
 
