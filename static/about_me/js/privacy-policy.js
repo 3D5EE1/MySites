be157c4743661e55e3a54e2fec0a354ad30f87fc
Evgeny-Gurin <rypylook@outlook.com>
2019-02-11 23:34:04 +0500

    v0.4.9581 (rebuild privacy-policy.js)

--- static/about_me/js/privacy-policy.js
+++ static/about_me/js/privacy-policy.js
@@ -1,20 +1,22 @@
-(function() {
+;(function () {
   'use strict';
 
-  var btnScrollDown = document.querySelector('#scroll_down');
-
-  function scrollDown() {
-    var windowCoords = document.documentElement.clientHeight;
-    (function scroll() {
-      if (window.pageYOffset < windowCoords) {
-        window.scrollBy(0, 20);
-        setTimeout(scroll, 0);
+  const btnScrollDown = document.querySelectorAll('.scroll_down');
+  for (let i of btnScrollDown) {
+      i.onclick = function scroll() {
+          if (window.pageYOffset < (i.dataset.px*1 - i.dataset.stopper*1)) {
+            window.scrollBy(0, i.dataset.speed*1);
+            setTimeout(scroll, 0);
+          } else if (window.pageYOffset < i.dataset.px*1) {
+            window.scrollBy(0, 1);
+            setTimeout(scroll, 0);
+          }
+          if (window.pageYOffset > i.dataset.px*1) {
+            window.scrollTo(0, i.dataset.px*1);
+          }
       }
-      if (window.pageYOffset > windowCoords) {
-        window.scrollTo(0, windowCoords);
-      }
-    })();
   }
+}());
 
-  btnScrollDown.addEventListener('click', scrollDown);
-})();
+
+
