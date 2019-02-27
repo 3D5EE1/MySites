1f56d21ebd46f9164b6fd5eaa37b67aab71849dd
Gurin-EI <rypylook@outlook.com>
2019-02-28 01:37:52 +0500

    v0.4.9777 (hidden block down function in header)

--- static/templates/js/base-header.js
+++ static/templates/js/base-header.js
@@ -7,10 +7,34 @@ window.onload = function() {
         return document.getElementsByClassName(siteClass);
     }
 
-    let mySites = $c('header-menu-my-sites');
-
-    mySites.onclick = function () {
-
+    function $s(siteSelector) {
+        return document.querySelector(siteSelector);
     }
 
+    let headerMenu = $s('div.header-menu');
+    let hiddenMenu = $s('div.hidden-menu');
+    let arrowMenu = $('arrow');
+
+    // let arrowUp = document.createTextNode("▲");
+    // let arrowDown = document.createTextNode("▼");
+    // arrowUP.appendChild(arrowUp);
+
+    function hidden () {
+        hiddenMenu.style.display = 'none';
+        arrowMenu.innerHTML = "▼"
+    }
+
+    function visible () {
+        hiddenMenu.style.display = 'block';
+        arrowMenu.innerHTML = "▲"
+    }
+
+    addEventListener('click', function(e) {
+        if (e.target === headerMenu) {
+            if (hiddenMenu.style.display === 'block') hidden();
+            else visible();
+        } else if (e.target !== hiddenMenu) {
+            hidden();
+        }
+    })
 };
